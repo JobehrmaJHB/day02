87a85b001fa6ae673b9418829654b18418d0d372
jobehrmaJHB <jobehrma@student.wethinkcode.co.za>
2020-07-17 14:18:37 +0200

    update

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -6,29 +6,36 @@
 /*   By: jobehrma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/08 16:45:55 by jobehrma          #+#    #+#             */
-/*   Updated: 2020/07/09 08:53:38 by jobehrma         ###   ########.fr       */
+/*   Updated: 2020/07/17 14:13:57 by jobehrma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-	void ft_putchar(char c) {
-		write(1, &c, 1);
+int ft_putchar(char c )
+{
+	write (1 ,&c, 1);
+	return(0);
+}
 
+void ft_print_alphabet(void)
+
+{
+	char letter;
+	letter = 'a';
+
+	while (letter <= 'z')
+	
+	{
+		ft_putchar(letter);
+				letter++;
 	}
+	
+}
 
-	void ft_print_alphabet(void) {
-		char c;
-
-		c = 'a';
-		while (c <= 'z'){
-				ft_putchar(c);
-				c++;
-		}
-	}
-
-	int		main(void) {
-
-		ft_print_alphabet();
-		return (0);
-	}
+	int main()
+{
+	ft_print_alphabet();
+	ft_putchar('\n');
+	return(0);
+}
