f4e40645fa6f9e5dd828ec4f66a77a3aed3518a9
jobehrmaJHB <jobehrma@student.wethinkcode.co.za>
2020-07-17 14:25:48 +0200

    removing of hiden files

--- ex03/ft_is_nagitive.c
+++ ex03/ft_is_nagitive.c
@@ -1,37 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*   ft_is_nagitive.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jobehrma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/09 11:50:33 by jobehrma          #+#    #+#             */
-/*   Updated: 2020/07/10 14:55:01 by jobehrma         ###   ########.fr       */
+/*   Created: 2020/07/10 08:08:38 by jobehrma          #+#    #+#             */
+/*   Updated: 2020/07/10 14:39:34 by jobehrma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_putchar(char c)	
+int ft_putchar(char c)
 {
 	write (1, &c, 1);
-	return(0);
-}
-void ft_print_numbers(void)
-{
-		char number;
-
-		    number = '0';
-			while (number <= '9')
-					{
-						ft_putchar(number);
-						number++;
-					}
+	return (0);
 }
 
-int			main() 
+void ft_is_negative(int n)
 {
-	ft_print_numbers();
+	if (n < 0)
+	{
+	ft_putchar( 'N');
+	}
+
+	else if(n > 0)
+	{
+	ft_putchar( 'P' );
+	}
+
+}
+int	main()
+{
+	ft_is_negative(-5);
 	ft_putchar('\n');
-	return (0);	
+	return (0);
 }
--- ex04/ft_print_comb.c
+++ ex04/ft_print_comb.c
@@ -1,37 +1,56 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jobehrma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/09 11:50:33 by jobehrma          #+#    #+#             */
-/*   Updated: 2020/07/10 14:55:01 by jobehrma         ###   ########.fr       */
+/*   Created: 2020/07/10 09:11:38 by jobehrma          #+#    #+#             */
+/*   Updated: 2020/07/10 16:47:57 by jobehrma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int ft_putchar(char c)	
+void ft_putchar(char a)
 {
-	write (1, &c, 1);
-	return(0);
-}
-void ft_print_numbers(void)
-{
-		char number;
-
-		    number = '0';
-			while (number <= '9')
-					{
-						ft_putchar(number);
-						number++;
-					}
+	write (1, &a, 1);
+	
 }
 
-int			main() 
+void ft_print_com(void)
 {
-	ft_print_numbers();
+	char a = '0';
+	char b = '0';
+	char c = '0';
+
+	while (a <= '9')
+	{
+		while (b <= '9')
+		{
+			while (c <= '9')
+			{
+				if (a < b && b < c)
+				{
+					ft_putchar (a);
+					ft_putchar (b);
+					ft_putchar (c);
+					ft_putchar(',');
+					ft_putchar(' ');
+				
+				}
+				c++;
+			}
+			c = '0';
+			b++;
+		}
+		b ='0';
+		a++;
+	}
+}
+int main()
+{
+	ft_print_com();
 	ft_putchar('\n');
-	return (0);	
+	return (0);
 }
